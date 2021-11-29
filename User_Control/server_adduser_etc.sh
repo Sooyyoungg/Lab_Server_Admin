581f27e7d1ae4d86e2ce6527b1fd6c4a11afbf2e
Sooyoung Kim <rlatndud0513@snu.ac.kr>
2021-11-29 13:54:35 +0900

    Create server_adduser_etc.sh

---
+++ User_Control/server_adduser_etc.sh
@@ -0,0 +1,15 @@
+# prepared env #this part need to exec only 1!
+# ./server_adduser_etc.sh 
+
+nodes=(master node1 node2 storage)
+for node in ${nodes[@]} 
+do
+	scp -r /etc/connectome conmaster@$node:~/server_management/connectome
+	ssh -A -T $node <<-EOF
+	sudo -S rm -rf /etc/connectome
+	sudo -S mkdir /etc/connectome
+	sudo -S mv ~/server_management/connectome /etc
+	sudo -S chown -R root:root /etc/connectome
+	rm -rf ~/server_management/connectome
+	EOF
+done
