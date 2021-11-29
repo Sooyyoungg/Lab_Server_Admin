658eb894c46d22d92431f9b57d5939bae0e11b79
Sooyoung Kim <rlatndud0513@snu.ac.kr>
2021-11-29 17:22:46 +0900

    Update server_addkey.sh

--- User_Control/server_addkey.sh
+++ User_Control/server_addkey.sh
@@ -7,24 +7,27 @@ cd /home/connectome/$userid
 sudo -S mkdir .ssh
 cd .ssh
 sudo -S touch authorized_keys
-echo $public_key >> authorized_keys
+sudo -S chmod 777 authorized_keys
+sudo -S echo $public_key >> authorized_keys
 sudo -S chgrp connectome authorized_keys
 sudo -S chown $userid authorized_keys
+sudo -S chmod 755 authorized_keys
 cd ..
 sudo -S chgrp connectome .ssh
 sudo -S chown $userid .ssh
 
 # add public key in other nodes
 nodes="master node1 node2 storage"
-for node in $nodes
-do
-        ssh -T -A conmaster@$node <<-EOF
+for node in $nodes; do
+ssh -A conmaster@$node <<-EOF
         bash
         cd /home/connectome/$userid
         sudo -S mkdir .ssh
         cd .ssh
         sudo -S touch authorized_keys
-        echo $public_key >> authorized_keys
+        sudo -S chmod 777 authorized_keys
+        sudo -S echo $public_key >> authorized_keys
+        sudo -S chmod 755 authorized_keys
         sudo -S chgrp connectome authorized_keys
         sudo -S chown $userid authorized_keys
         cd ..
