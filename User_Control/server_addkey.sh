0cc8b956741ed1d05810b180742c358c19a64dfb
Sooyoung Kim <rlatndud0513@snu.ac.kr>
2021-11-29 13:49:52 +0900

    Create server_addkey.sh

---
+++ User_Control/server_addkey.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+userid=$1
+public_key=$2
+
+# add public key in gateway
+cd /home/connectome/$userid
+sudo -S mkdir .ssh
+cd .ssh
+sudo -S touch authorized_keys
+echo $public_key >> authorized_keys
+sudo -S chgrp connectome authorized_keys
+sudo -S chown $userid authorized_keys
+cd ..
+sudo -S chgrp connectome .ssh
+sudo -S chown $userid .ssh
+
+# add public key in other nodes
+nodes="master node1 node2 storage"
+for node in $nodes
+do
+        ssh -T -A conmaster@$node <<-EOF
+        bash
+        cd /home/connectome/$userid
+        sudo -S mkdir .ssh
+        cd .ssh
+        sudo -S touch authorized_keys
+        echo $public_key >> authorized_keys
+        sudo -S chgrp connectome authorized_keys
+        sudo -S chown $userid authorized_keys
+        cd ..
+        sudo -S chgrp connectome .ssh
+        sudo -S chown $userid .ssh
+EOF
+done
